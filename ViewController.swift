23a6824e0c6fa813e1e251e5d4167a44d3c2dfbc
ImanF01 <imanfahmy01@gmail.com>
2017-07-12 15:43:41 -0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TestingContainerView
+//
+//  Created by Iman F on 7/12/17.
+//  Copyright © 2017 Iman F (group project). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func delveButtonTapped(_ sender: UIButton) {
+        print("Delve button tapped")
+    }
+    @IBAction func followButtonTapped(_ sender: UIButton) {
+        print("Follow button tapped")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
